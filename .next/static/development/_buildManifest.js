b0b5f3557ce599700fd299840d6fb2d5d81a8b16
Josh Jacobson <joshjacobson14@gmail.com>
2020-08-22 15:35:56 -0700

    update gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/music":["static\u002Fchunks\u002Fpages\u002Fmusic.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
